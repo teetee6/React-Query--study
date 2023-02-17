43bf602c65362a55ec45998f463e2becf64aa606
Hyoshik Lee <hyoslee@cx2r4s4.42seoul.kr>
2023-02-17 20:26:56 +0900

    ;

---
+++ section3_/base-lazy-days/client/src/components/app/App.tsx
@@ -0,0 +1,21 @@
+import { ChakraProvider } from '@chakra-ui/react';
+import { ReactElement } from 'react';
+import { QueryClientProvider } from 'react-query';
+
+import { queryClient } from '../../react-query/queryClient';
+import { theme } from '../../theme';
+import { Loading } from './Loading';
+import { Navbar } from './Navbar';
+import { Routes } from './Routes';
+
+export function App(): ReactElement {
+  return (
+    <ChakraProvider theme={theme}>
+      <QueryClientProvider client={queryClient}>
+        <Navbar />
+        <Loading />
+        <Routes />
+      </QueryClientProvider>
+    </ChakraProvider>
+  );
+}
